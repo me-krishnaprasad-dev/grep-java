7744dc8ab17085bbaee9039086438395a40546ef
me-krishnaprasad-dev <me.krishnaprasad.dev@gmail.com>
2025-10-21 23:18:31 +0530

    feat: added digit matching in the match pattern method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,8 @@ public class Main {
   public static boolean matchPattern(String inputLine, String pattern) {
     if (pattern.length() == 1) {
       return inputLine.contains(pattern);
+    } else if(pattern.equals("\\d")) {
+        return inputLine.chars().anyMatch(Character::isDigit);
     } else {
       throw new RuntimeException("Unhandled pattern: " + pattern);
     }
